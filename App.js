a69b56e0c320b0e2987790ef74fe4acd178ebd57
Rapuska <118633578+Rapuska@users.noreply.github.com>
2024-09-07 09:29:57 +0300

    Finished viikkotehtava2

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Modal, Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      {!modalVisible && (
+        <View style={styles.homeView}>
+          <Pressable
+            onPress={() => setModalVisible(true)}>
+            <Text style={styles.homeText}>Show Modal message</Text>
+          </Pressable>
+        </View>
+      )}
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>This is modal...</Text>
+            <Pressable
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.modalCloseText}>Close</Text>
+            </Pressable>
+          </View>
+      </Modal>
+
     </View>
   );
 }
@@ -13,8 +40,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+
+    homeView: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      },
+      homeText: {
+        fontSize: 16,
+      },
+
+    modalView: { 
+      backgroundColor: "#fbfbfb", 
+      marginTop: 240,
+      padding: 24, 
+      alignItems: "center", 
+      elevation: 3, 
+      width: '100%',
+      },
+      modalText: {
+        marginBottom: 24,
+        textAlign: "center",
+        fontSize: 16,
+      },
+      modalCloseText: {
+        marginTop: 24,
+        textAlign: "center",
+        fontSize: 16,
+      },
+
 });
